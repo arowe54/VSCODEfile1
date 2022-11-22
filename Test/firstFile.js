91eeed75b26c2cf8fcefc071f0049603b5baf5b7
arowe54 <arowe54@uwo.ca>
2022-11-21 19:54:18 -0500

    Variables in .js

---
+++ Test/firstFile.js
@@ -0,0 +1,60 @@
+let message; // declares a variable called message
+message = 'Hello'; // stores String in message
+
+alert(message); // prints message to screen
+
+let user = 'John Rowe';
+age = 25;
+message = 'Hello';
+
+// can also do this
+let user = 'Andrew Rowe',
+    age = 18,
+    message = 'hello';
+
+// var is basically the same as let
+
+let hello = 'Hello World';
+message = hello; // copies 'Hello World' from hello into message
+alert(hello);
+alert(message); // prints 'Hello World' twice
+
+// variables can only be letters, digits, $ or _
+// cannot start with a digit
+// use camelCase
+
+// can use any language, including cryllic and chinese symbols. should use english (national convention)
+
+// let, class, return, and function are reserved (cannot use)
+
+/*
+older versions of js could use variables without defining them.
+If you put 'use strict', you have to declare variables
+*/
+
+// constants
+const myBirthday = '16.06.2004';
+
+// constants are good for difficult to remember values (ex. colours in hexadecimal)
+const COLOUR_RED = "#F00";
+const COLOUR_GREEN = "#0F0";
+const COLOUR_BLUE = "#00F";
+const COLOUR_ORANGE = "#FF7F00";
+let colourGreen = COLOR_GREEN;
+alert(colourGreen)
+
+// if it is not known before loading the file, use regular camelCase
+
+// variable advice from https://javascript.info/variables
+/*
+Use readable names,
+stay away from abbreviations / short names (ex. a, b)
+make it descriptive and concise
+make it make sense (ex. if a user is a user, name variables like newUser instead of newVisitor)
+use different variables for different values so debugging is easier
+*/
+
+
+// operators are the basically the same as java
+// ** are for exponents
+
